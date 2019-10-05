74aa661cee819215beb6e463088800d2b2c87a5a
enzomx <moises@enzosoluciones.net>
2019-10-04 19:35:58 -0500

    MYSQL CONECTOR MYSQLDB

--- Pyantae/settings.py
+++ Pyantae/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'Pyantae.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'enzomx_pyantae',
+		'USER': 'enzomx_pyantae',
+		'PASSWORD': "6+2t05{OUlok",
+		'HOST': "143.95.235.141",
+		'PORT': "3306",
+'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+}
+		}
 }
 
 
--- read.py
+++
@@ -1,9 +0,0 @@
-import time
-import serial
-
-
-arduino = serial.Serial('COM2', 9600)
-time.sleep(2)
-rawString = arduino.readline()
-print(rawString)
-arduino.close()
